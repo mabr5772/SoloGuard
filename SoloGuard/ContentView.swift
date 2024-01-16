048ae14c0a4db770555652669aac8856d80463a4
jich7678 <jich7678@colorado.edu>
2024-01-16 15:25:12 -0700

    Initial Commit

---
+++ SoloGuard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoloGuard
+//
+//  Created by Sam Chen on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoloGuard/SoloGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloGuardApp.swift
+//  SoloGuard
+//
+//  Created by Sam Chen on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SoloGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
